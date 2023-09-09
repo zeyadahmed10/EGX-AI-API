930fda909f1e6831c334f6c39361dba241539576
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-09-09 15:46:50 +0300

    adding equities.txt path to StockService

--- scraping-service/src/main/java/org/egx/scraping/services/StockService.java
+++ scraping-service/src/main/java/org/egx/scraping/services/StockService.java
@@ -5,6 +5,7 @@ import org.egx.scraping.IO.Stock;
 import org.egx.scraping.scrapers.StockScraper;
 import org.egx.scraping.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,7 +20,7 @@ public class StockService {
     public static final String BASE_URL = "https://www.mubasher.info/markets/EGX/stocks/";
     public List<Stock> getUpdatedStockList() throws IOException {
         List<Stock> stocks = new ArrayList<Stock>();
-        var equities = Utils.readEquities();
+        var equities = Utils.readEquities("equities.txt");
         for(var eq: equities){
             var document = stockScraper.getDocument(BASE_URL+eq.get(3));
             var stock = stockScraper.parseDataOnPage(document);
