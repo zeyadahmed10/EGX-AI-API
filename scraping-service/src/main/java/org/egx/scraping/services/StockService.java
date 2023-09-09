5851f1b5330abb6bb5a64375adebb39cbaf58f88
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-09-09 15:41:28 +0300

    adding StockScraper DI to StockService.java

--- scraping-service/src/main/java/org/egx/scraping/services/StockService.java
+++ scraping-service/src/main/java/org/egx/scraping/services/StockService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.egx.scraping.IO.Stock;
 import org.egx.scraping.scrapers.StockScraper;
 import org.egx.scraping.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -13,23 +14,18 @@ import java.util.List;
 @Service
 @Slf4j
 public class StockService {
+    @Autowired
+    private StockScraper stockScraper;
     public static final String BASE_URL = "https://www.mubasher.info/markets/EGX/stocks/";
     public List<Stock> getUpdatedStockList() throws IOException {
         List<Stock> stocks = new ArrayList<Stock>();
         var equities = Utils.readEquities();
-        int i = 0;
         for(var eq: equities){
-            System.out.printf("%d , %s \n",i,BASE_URL+eq.get(3));
-            if(i<159){
-                i++;
-                continue;
-            }
-            var document = StockScraper.getDocument(BASE_URL+eq.get(3));
-            var stock = StockScraper.parseDataOnPage(document);
+            var document = stockScraper.getDocument(BASE_URL+eq.get(3));
+            var stock = stockScraper.parseDataOnPage(document);
             stock.setReutersCode(eq.get(3));
             log.info(stock.toString());
             stocks.add(stock);
-            i++;
         }
         return stocks;
     }
