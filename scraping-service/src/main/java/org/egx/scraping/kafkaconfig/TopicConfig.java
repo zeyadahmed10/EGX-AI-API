f8f1a0b8f07ccf19cdb250c4056c0a2caeae66ae
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-09-10 17:19:41 +0300

    add scaping-service/TopicConfig.java
    create two topics scrapedNews and scrapedStocks

---
+++ scraping-service/src/main/java/org/egx/scraping/kafkaconfig/TopicConfig.java
@@ -0,0 +1,19 @@
+package org.egx.scraping.kafkaconfig;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class TopicConfig {
+
+    @Bean
+    public NewTopic scrapedNewsTopic(){
+        return TopicBuilder.name("scrapedNews").build();
+    }
+    @Bean
+    public NewTopic scrapedStocksTopic(){
+        return TopicBuilder.name("scrapedStocks").build();
+    }
+}
