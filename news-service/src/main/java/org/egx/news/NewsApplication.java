999826c63be2197ecc7533fe6b4404c1e5e8b3b6
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-09-18 10:49:59 +0300

    adding main function to news-service

--- news-service/src/main/java/org/egx/news/NewsApplication.java
+++ news-service/src/main/java/org/egx/news/NewsApplication.java
@@ -1,4 +1,11 @@
 package org.egx.news;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NewsApplication {
+    public static void main(String []args){
+        SpringApplication.run(NewsApplication.class, args);
+    }
 }
