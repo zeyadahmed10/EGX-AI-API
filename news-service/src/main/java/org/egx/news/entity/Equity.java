71ccb90061d72e5bbd49f281059547d3ac932fe7
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-09-18 15:45:54 +0300

    adding post construct method for adding all equities

--- news-service/src/main/java/org/egx/news/entity/Equity.java
+++ news-service/src/main/java/org/egx/news/entity/Equity.java
@@ -1,12 +1,18 @@
 package org.egx.news.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Equity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
--- news-service/src/main/java/org/egx/news/services/EquityService.java
+++ news-service/src/main/java/org/egx/news/services/EquityService.java
@@ -1,11 +1,16 @@
 package org.egx.news.services;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.egx.news.entity.Equity;
 import org.egx.news.exceptions.ResourceNotFoundException;
 import org.egx.news.repos.EquityRepository;
+import org.egx.news.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,7 +18,25 @@ public class EquityService {
 
     @Autowired
     private EquityRepository equityRepository;
-
+    //adding the equities
+    @PostConstruct
+    public void init() {
+        var count = equityRepository.count();
+        if(count!=0)
+            return;
+        var equities = Utils.readEquities("equities.txt");
+        List<Equity> entityEquities = new ArrayList<>();
+        for(var eq: equities){
+            var equity = Equity.builder()
+                    .ISN(eq.get(0))
+                    .name(eq.get(1))
+                    .sector(eq.get(2))
+                    .reutersCode(eq.get(3))
+                    .listingDate(eq.get(4)).build();
+            entityEquities.add(equity);
+        }
+        equityRepository.saveAll(entityEquities);
+    }
     public Equity getEquityByCode(String reutersCode){
         return equityRepository.findByReutersCode(reutersCode).orElseThrow(()->{
             log.error("could not find equity by code: "+reutersCode);
