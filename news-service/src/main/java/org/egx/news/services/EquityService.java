512f7bc4c992c0fa57cae15663c4efbedeee63a1
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-10-23 11:42:59 +0300

    adding flyway support and init-database and equity scripts

--- news-service/src/main/java/org/egx/news/services/EquityService.java
+++ news-service/src/main/java/org/egx/news/services/EquityService.java
@@ -2,12 +2,10 @@ package org.egx.news.services;
 
 import exceptions.PageExceeding;
 import exceptions.ResourceNotFoundException;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.egx.news.entity.Equity;
 import org.egx.news.entity.News;
 import org.egx.news.repos.EquityRepository;
-import org.egx.news.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -15,7 +13,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,25 +22,25 @@ public class EquityService {
     @Autowired
     private EquityRepository equityRepository;
     //adding the equities
-    @PostConstruct
-    public void init() {
-        var count = equityRepository.count();
-        if(count!=0)
-            return;
-        var equities = Utils.readEquities("equities.txt");
-        List<Equity> entityEquities = new ArrayList<>();
-        for(var eq: equities){
-            var equity = Equity.builder()
-                    .ISN(eq.get(0))
-                    .name(eq.get(1))
-                    .sector(eq.get(2))
-                    .reutersCode(eq.get(3))
-                    .listingDate(eq.get(4)).build();
-            entityEquities.add(equity);
-        }
-        equityRepository.saveAll(entityEquities);
-        log.info("Successfully saved equities to DB");
-    }
+//    @PostConstruct
+//    public void init() {
+//        var count = equityRepository.count();
+//        if(count!=0)
+//            return;
+//        var equities = Utils.readEquities("equities.txt");
+//        List<Equity> entityEquities = new ArrayList<>();
+//        for(var eq: equities){
+//            var equity = Equity.builder()
+//                    .ISN(eq.get(0))
+//                    .name(eq.get(1))
+//                    .sector(eq.get(2))
+//                    .reutersCode(eq.get(3))
+//                    .listingDate(eq.get(4)).build();
+//            entityEquities.add(equity);
+//        }
+//        equityRepository.saveAll(entityEquities);
+//        log.info("Successfully saved equities to DB");
+//    }
     public Page<Equity> fetchEquitiesAsList(String sectorFilter, String nameFilter, int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         return equityRepository.findAllByFilters(sectorFilter, nameFilter, pageable);
---
+++ news-service/src/main/resources/db/migration/V2__inti_database.sql
@@ -0,0 +1,23 @@
+create sequence if not exists news_seq start with 1 increment by 50;
+create sequence if not exists equity_seq start with 1 increment by 50;
+create table if not exists news(
+
+ id integer not null,
+ reuters_code varchar(255) not null,
+ title varchar(500) not null,
+ article text,
+ time timestamp(6) not null,
+ equity_id integer,
+ primary key(id)
+);
+create table if not exists equity
+(
+    id integer not null,
+    isn varchar(255),
+    listing_date varchar(255),
+    name varchar(255),
+    reuters_code varchar(255),
+    sector varchar(255),
+    primary key (id)
+);
+alter table if exists news add constraint FK_EquityNews foreign key (equity_id) references equity;
