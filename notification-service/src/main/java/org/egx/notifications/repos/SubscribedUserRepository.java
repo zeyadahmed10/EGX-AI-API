232d9c77ec54821a3dd418bc78713e1054ae95e3
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-11-08 12:24:23 +0200

    creating subscribedUsersRepo

---
+++ notification-service/src/main/java/org/egx/notifications/repos/SubscribedUserRepository.java
@@ -0,0 +1,4 @@
+package org.egx.notifications.repos;
+
+public interface SubscribedUserRepository extends org.springframework.data.jpa.repository.JpaRepository<org.egx.notifications.entity.SubscribedUser, java.lang.Integer> {
+}
