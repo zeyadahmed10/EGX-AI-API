05c3fccc8059347495a52e1b97c93b935d23ca8b
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-11-09 18:32:56 +0200

    adding native query to find subscribed users in stock and news in subscribedUserRepository

--- notification-service/src/main/java/org/egx/notifications/repos/SubscribedUserRepository.java
+++ notification-service/src/main/java/org/egx/notifications/repos/SubscribedUserRepository.java
@@ -9,13 +9,21 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SubscribedUserRepository extends JpaRepository<SubscribedUser, Integer> {
-    String GET_SUB_USERS_QUERY = "select su.* from equity e \n" +
+    String GET_SSUB_USERS_QUERY = "select su.* from equity e \n" +
             "join stock_subscription as ss\n" +
             "on ss.equity_id = e.id\n" +
             "join subscribed_user as su\n" +
             "on su.id=ss.user_id\n" +
             "where e.reuters_code = :codeParam ;";
+    String GET_NSUB_USERS_QUERY = "select su.* from equity e \n" +
+            "join news_subscription as ns\n" +
+            "on ns.equity_id = e.id\n" +
+            "join subscribed_user as su\n" +
+            "on su.id=ns.user_id\n" +
+            "where e.reuters_code = :codeParam ;";
     Optional<SubscribedUser> findByEmail(String email);
-    @Query(value = GET_SUB_USERS_QUERY, nativeQuery = true)
+    @Query(value = GET_SSUB_USERS_QUERY, nativeQuery = true)
     List<SubscribedUser> findStockSubscribedUsersByReutersCode(@Param("codeParam") String reutersCode);
+    @Query(value = GET_NSUB_USERS_QUERY, nativeQuery = true)
+    List<SubscribedUser> findNewsSubscribedUsersByReutersCode(@Param("codeParam") String reutersCode);
 }
