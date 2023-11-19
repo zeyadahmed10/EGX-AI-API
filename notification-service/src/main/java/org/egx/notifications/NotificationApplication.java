99fbdbcd8b0e3c3fa6461d147166cabe168c5f0b
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-11-19 19:28:20 +0200

    fixing configuration to email properties in notification service

--- notification-service/src/main/java/org/egx/notifications/NotificationApplication.java
+++ notification-service/src/main/java/org/egx/notifications/NotificationApplication.java
@@ -14,18 +14,10 @@ public class NotificationApplication {
     }
 
 //    @Bean
-//    CommandLineRunner commandLineRunner(StockSubscriptionService service, EquityRepository repo, SubscribedUserRepository repo2){
+//    CommandLineRunner commandLineRunner(EmailService service){
 //        return args ->{
-//            repo.save(Equity.builder().ISN("isn").name("test").listingDate("date").sector("sector").reutersCode("test").build());
-//            repo.save(Equity.builder().ISN("isn").name("test2").listingDate("date").sector("sector").reutersCode("test2").build());
-//            service.subscribe("zeyad@gmail.com",new SubscriptionRequest("test","stock"));
-//            service.subscribe("hesham@gmail.com",new SubscriptionRequest("test","stock"));
-//            service.subscribe("zeyad2@gmail.com",new SubscriptionRequest("test2","stock"));
-//            service.subscribe("hesham2@gmail.com",new SubscriptionRequest("test2","stock"));
-//            System.out.println("test");
-//            System.out.println(repo2.findSubscribedUsersByReutersCode("test"));
-//            System.out.println("test2");
-//            System.out.println(repo2.findSubscribedUsersByReutersCode("test2"));
+//                service.sendEmail("test@gmail.com","notification service testing","Hello Zeyad, from springboot app");
+//            System.out.println("we are here");
 //        };
 //    }
 }
