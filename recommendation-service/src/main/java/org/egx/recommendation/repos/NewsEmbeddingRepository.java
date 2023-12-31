3189ea10643d709880757c2aef874854884479ad
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-12-31 12:06:54 +0200

    creating findKNearesetNeighbors function and native query

--- recommendation-service/src/main/java/org/egx/recommendation/repos/NewsEmbeddingRepository.java
+++ recommendation-service/src/main/java/org/egx/recommendation/repos/NewsEmbeddingRepository.java
@@ -4,8 +4,15 @@ import org.egx.recommendation.entity.NewsEmbedding;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NewsEmbeddingRepository extends JpaRepository<NewsEmbedding, Integer> {
-
+    String KNearestNeighborsQuery = "select * from news_embedding ne order by ne.embedding <-> " +
+            "(select us.embedding from user_history us where us.email= :emailParam ) LIMIT :kParam";
     Page<NewsEmbedding> findAllByHitsDesc(Pageable pageable);
+    @Query(nativeQuery = true, value = KNearestNeighborsQuery)
+    List<NewsEmbedding> findKNearestNeighbors(@Param("emailParam")String userEmail, @Param("kParam") int k);
 }
