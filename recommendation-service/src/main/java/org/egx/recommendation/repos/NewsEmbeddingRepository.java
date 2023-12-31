3dbe49c63a5e91cc5890852dd8f95f6bf4003dc3
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-12-31 11:51:51 +0200

    create findAllByHitsMethods

--- recommendation-service/src/main/java/org/egx/recommendation/repos/NewsEmbeddingRepository.java
+++ recommendation-service/src/main/java/org/egx/recommendation/repos/NewsEmbeddingRepository.java
@@ -1,7 +1,11 @@
 package org.egx.recommendation.repos;
 
 import org.egx.recommendation.entity.NewsEmbedding;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsEmbeddingRepository extends JpaRepository<NewsEmbedding, Integer> {
+
+    Page<NewsEmbedding> findAllByHitsDesc(Pageable pageable);
 }
