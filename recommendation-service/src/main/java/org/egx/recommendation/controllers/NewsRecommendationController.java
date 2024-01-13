d93dd77b6d106be330dd2f8a1d6bae2825666274
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-13 10:08:10 +0200

    Adding Documentation to NewsRecommendationController

--- recommendation-service/src/main/java/org/egx/recommendation/controllers/NewsRecommendationController.java
+++ recommendation-service/src/main/java/org/egx/recommendation/controllers/NewsRecommendationController.java
@@ -1,5 +1,10 @@
 package org.egx.recommendation.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.egx.clients.io.NewsDto;
 import org.egx.recommendation.services.NewsRecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/recommendations")
+@Tag(name = "News Recommendation Controller", description = "APIs for getting news recommendations")
 public class NewsRecommendationController {
     @Autowired
     private NewsRecommendationService newsRecommendationService;
+    @Operation(
+            summary = "Get news recommendations",
+            description = "Get news recommendations for users based on their reading behavior. If the user is new and has no reading history, it recommends popular news."
+    )
     @GetMapping
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class)))
     Page<NewsDto> getNewsRecommendations(@RequestParam(defaultValue = "0") int page,
                                          @RequestParam(defaultValue = "5") int size, @AuthenticationPrincipal Jwt jwt){
         String userEmail = String.valueOf(jwt.getClaims().get("email"));
