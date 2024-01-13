14959e191ecc9b204c03441bdffbdee301534123
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-13 09:47:56 +0200

    adding dev volumes to docker-compose-dev.yml and Adding Swagger OpenAPI to Stock service and document it

--- stocks-service/src/main/java/org/egx/stocks/controllers/HistoricalStocksController.java
+++ stocks-service/src/main/java/org/egx/stocks/controllers/HistoricalStocksController.java
@@ -1,5 +1,10 @@
 package org.egx.stocks.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.egx.stocks.entity.OHCLVStatistics;
 import org.egx.stocks.services.HistoricalStockService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/historical-stocks")
+@Tag(name = "Historical Stocks Controller", description = "APIs for retrieving historical stock data")
 public class HistoricalStocksController {
 
     @Autowired
@@ -18,7 +24,12 @@ public class HistoricalStocksController {
     1 week 30 minutes period
     1 month 1 day period
     * */
+    @Operation(
+            summary = "Get OHCLV statistics for a stock",
+            description = "Get OHCLV (Open, High, Close, Low, Volume) statistics for a stock based on specified parameters"
+    )
     @GetMapping("/{reutersCode}")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class)))
     Page<OHCLVStatistics> getOHCLVStatistics(
             @PathVariable("reutersCode") String reutersCode,
             @RequestParam(defaultValue = "30 minutes") String periodParam,
--- stocks-service/src/main/java/org/egx/stocks/controllers/StockController.java
+++ stocks-service/src/main/java/org/egx/stocks/controllers/StockController.java
@@ -1,5 +1,10 @@
 package org.egx.stocks.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.egx.stocks.entity.UpdatedStock;
 import org.egx.stocks.services.StockService;
@@ -10,12 +15,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/stocks")
+@Tag(name = "Stocks Controller", description = "The Updated Stocks API")
 public class StockController {
 
     @Autowired
     private StockService stockService;
-
+    @Operation(
+            summary = "Fetch all updated stocks",
+            description = "Fetch all updated stocks entities with their specified equity"
+    )
     @GetMapping
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class)))
     public Page<UpdatedStock> fetchAllStocks(
             @RequestParam(defaultValue = "") String sectorFilter,
             @RequestParam(defaultValue = "") String nameFilter,
@@ -24,7 +34,13 @@ public class StockController {
     ){
         return stockService.fetchAllStocks(sectorFilter, nameFilter,  page, size);
     }
-    @GetMapping("{reutersCode}")
+    @Operation(
+            summary = "Get updated stock by Reuters code",
+            description = "Get an updated stock entity by its Reuters code"
+    )
+    @GetMapping("/{reutersCode}")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UpdatedStock.class)))
+    @ApiResponse(responseCode = "404", description = "Stock not found")
     public UpdatedStock getUpdatedStockByReutersCode(@PathVariable String reutersCode){
         return stockService.getStockByReutersCode(reutersCode);
     }
